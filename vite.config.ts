5893fdf94134cd202ae3bb2e9c12d206038c6a69
Mukesh-15 <mukeshdoodapaka@gmail.com>
2025-06-06 22:39:35 +0530

    Fix SPA routing for Vercl

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
