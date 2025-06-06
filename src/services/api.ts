f59f6b62f8200b6f448f94101a5e0c6a8d526a41
Mukesh-15 <mukeshdoodapaka@gmail.com>
2025-06-06 22:43:38 +0530

    Fix SPA routing for Vrcel

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://backend-xdjn.onrender.com/api';
 
 // Create axios instance
 const api: AxiosInstance = axios.create({
